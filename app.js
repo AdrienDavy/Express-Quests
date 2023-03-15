29476e5c88db483911d532129d49b9852d84b354
Adrien Davy <adriendavyfx@gmail.com>
2023-03-15 15:50:53 +0100

    quest 2 done

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require("express");
-
+const database = require("./database");
+const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
+require("dotenv").config();
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favourite movie liste");
 };
 
-app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+app.get("/", welcome);
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUsersById);
+
+
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
